7966674ad22fe27b8e7f6a3b4d90b792d3e6fc0e
ouyangshicheng <312286556@qq.com>
2021-09-29 15:12:36 +0800

    所有分页查询增加排序

--- oysc/src/main/java/com/yimi/oysc/vo/common/PageWrapper.java
+++ oysc/src/main/java/com/yimi/oysc/vo/common/PageWrapper.java
@@ -1,5 +1,6 @@
 package com.yimi.oysc.vo.common;
 
+import com.baomidou.mybatisplus.core.metadata.OrderItem;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
@@ -8,6 +9,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Auto-Generated
@@ -29,10 +32,27 @@ public class PageWrapper<T> {
     @ApiModelProperty("当前是第多少页")
     private Integer currentPage = 1;
 
+    @ApiModelProperty("升序排序字段")
+    private List<String> ascColumnList;
+
+    @ApiModelProperty("降序排序字段")
+    private List<String> descColumnList;
+
     public <T> Page<T> toDbPage() {
         Page<T> page = new Page<T>();
         page.setSize(this.pageSize);
         page.setCurrent(this.currentPage);
+
+        if (ascColumnList != null) {
+            List<OrderItem> list = ascColumnList.stream().map(OrderItem::asc).collect(Collectors.toList());
+            page.addOrder(list);
+        }
+        if (descColumnList != null) {
+            List<OrderItem> list = descColumnList.stream().map(OrderItem::desc).collect(Collectors.toList());
+            page.addOrder(list);
+        }
+
+
         return page;
     }
 }
